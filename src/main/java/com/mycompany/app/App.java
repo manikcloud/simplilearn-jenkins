cf255e9c904825783c79a745776fadaa1f5d8b04
Varun <varunmanik1@gmail.com>
2022-04-10 16:53:51 +0000

    my java app v2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "My integration between Jenkins, git & maven is done !!" );
     }
 }
